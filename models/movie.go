909a62301ccacf22bda20dedd9d2bb698f92048b
arlan <elfqqz@gmail.com>
2026-02-02 12:52:09 +0500

    making a movie model and create a handler

--- models/movie.go
+++ models/movie.go
@@ -2,6 +2,7 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+//муви модель
 type Movie struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Title    string             `bson:"title"         json:"title"`
