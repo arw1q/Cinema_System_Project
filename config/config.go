a2718ba110ba4412b76c2da656bc150f5d02673e
Baxadu1 <bakyt.kuanysh6@gmail.com>
2026-02-02 12:52:10 +0500

    Update database and config

--- config/config.go
+++ config/config.go
@@ -1,31 +1,31 @@
 package config
 
 import (
- "context"
- "log"
- "time"
+	"context"
+	"log"
+	"time"
 
- "go.mongodb.org/mongo-driver/mongo"
- "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var DB *mongo.Database
 
-
-
 func ConnectDB() {
- ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
- defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
- client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
- if err != nil {
-  log.Fatal(err)
- }
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
- if err := client.Ping(ctx, nil); err != nil {
-  log.Fatal("MongoDB ping failed:", err)
- }
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("MongoDB ping failed:", err)
+	}
 
- DB = client.Database("cinema_system")
- log.Println("Connected to MongoDB")
+	DB = client.Database("cinema_system")
+	log.Println("Connected to MongoDB")
 }
+
+//change
